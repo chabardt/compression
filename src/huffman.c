58685007bd299297f7b057a94e4c97d56677b063
Théodore Chabardès <theodore@chabardes.net>
2013-09-04 13:28:02 +0200

    Some improvements

--- src/huffman.c
+++ src/huffman.c
@@ -6,18 +6,18 @@
 #include "../compression.h"
 
 typedef struct {
-	void* s ; // symbol
+	char* s ; // symbol
 	char* k ; // translation
 	int l ; // length of translation
 	int occurence ;
 } entry ;
 
-entry* entry_new (int symbol_size, void* _s, char* _k, int _l, int _o) {
+entry* entry_new (int symbol_size, char* _s, char* _k, int _l, int _o) {
 	entry *e = NULL ;
 	e = (entry*) malloc (sizeof (*e) ) ;
 	assert (e != NULL) ;
-	e->s = (void*) malloc (symbol_size) ;
-	e->k = (void*) malloc (_l) ;
+	e->s = (char*) malloc (symbol_size) ;
+	e->k = (char*) malloc (_l) ;
 	memcpy (e->s, _s, symbol_size) ; memcpy (e->k, _k, _l) ; e->l = _l ; e->occurence = _o ;
 	return e;
 }
@@ -37,17 +37,17 @@ int entry_cmp (const void* e1, const void *e2) {
 typedef struct {
 	int sigma_size ;
 	int symbol_size ;
-	pqueue* table ;
+	pqueue* table ; // priority queue of entries.
 } code ;
 
-void forward_analyze (code* c, void** alphabet, int alphabet_size, int symbol_size, void* (*read) ());
-void huffman_tree (code* c, void ** alphabet, int alphabet_size, int symbol_size) ;
+void forward_analyze (code* c, char* alphabet, int alphabet_size, int symbol_size, void (*read) (char*));
+void huffman_tree (code* c, char* alphabet, int alphabet_size, int symbol_size) ;
 char* code_to_str (code c) ;
 char* look_up_translation (char* symbol, code c) ;
 
-void huffman_encoding (void** alphabet, int alphabet_size, int symbol_size, int size, void* (*read) (), void (*write) (char*), void (*rewind) ()) {
+void huffman_encoding (char* alphabet, int alphabet_size, int symbol_size, void (*read) (char*), void (*write) (char*), void (*rewind) ()) {
 	char* translation ;
-	void* symbol = NULL ;
+	char* symbol = malloc (symbol_size*sizeof(char)) ;
 
 	code c ;
 	forward_analyze (&c, alphabet, alphabet_size, symbol_size, read) ;
@@ -56,36 +56,42 @@ void huffman_encoding (void** alphabet, int alphabet_size, int symbol_size, int 
 
 	rewind () ;
 
-	while ((symbol = read () ) != NULL) {
+	read (symbol) ;
+	while (symbol != NULL) {
 		translation = look_up_translation(symbol, c) ;
 		write (translation) ;
+		read (symbol) ;
 	}
 }
 
-void read_code (code *c, void* (*read) ()) ;
-void translate (code c, void* (*read) (), void (*write) ()) ;
+void read_code (code *c, void (*read) (char*)) ;
+void translate (code c, void (*read) (char*), void (*write) (char*)) ;
 
-void huffman_decoding (void* (*read) (), void (*write) (char*)) {
+void huffman_decoding (void (*read) (char*), void (*write) (char*)) {
 	code c ;
 	read_code (&c, read) ;
 	translate (c, read, write) ;
 }
 
-void forward_analyze (code* c, void** alphabet, int alphabet_size, int symbol_size, void* (*read) ()) {
+void forward_analyze (code* c, char* alphabet, int alphabet_size, int symbol_size, void (*read) (char*)) {
 	entry** array = calloc (alphabet_size, sizeof(entry*)) ; // use of a array to avoid successiv insertion in pqueue. Plus we can reduced the size of the alphabet with only the used symbols.
-	void* symbol = NULL ;
+	char* symbol = malloc (symbol_size*sizeof(char)) ;
 	int i=0 ; // used for looping.
-	while ((symbol = read ()) != NULL) {
-		while (i < alphabet_size && memcmp (symbol, alphabet[i], symbol_size) == 0) {
+	int result_cmp = 0;
+	read (symbol) ;
+	while (symbol != EOF) {
+		fprintf (stderr, "%c\n", symbol) ;
+		while (i < alphabet_size*symbol_size && (result_cmp = memcmp (symbol, alphabet+i*symbol_size, symbol_size)) == 0) {
 			i++;
 		}
-		if (memcmp (symbol, alphabet[i], symbol_size) == 0) {
+		if (result_cmp == 0) {
 			if (array[i] == NULL) {
 				array[i] = entry_new(symbol_size, symbol, "", 0, 1);
 			} else {
 				array[i]->occurence ++ ;
 			}
 		}
+		read (symbol) ;
 	}
 
 	c->symbol_size = symbol_size;
@@ -94,7 +100,7 @@ void forward_analyze (code* c, void** alphabet, int alphabet_size, int symbol_si
 
 }
 
-void huffman_tree (code* c, void ** alphabet, int alphabet_size, int symbol_size) {
+void huffman_tree (code* c, char* alphabet, int alphabet_size, int symbol_size) {
 	pqueue merged_trees;
 	pqueue single_node_trees;
 
@@ -109,11 +115,11 @@ char* look_up_translation (char* symbol, code c) {
 	return NULL;
 }
 
-void read_code (code *c, void* (*read) ()) {
+void read_code (code *c, void (*read) (char*)) {
 
 }
 
-void translate (code c, void* (*read) (), void (*write) ()) {
+void translate (code c, void (*read) (char*), void (*write) ()) {
 
 }
 
--- test/huffman.c
+++ test/huffman.c
@@ -1,5 +1,40 @@
 #include "../compression.h"
+#include <stdio.h>
+#include <string.h>
 
-int main () {
+FILE* fp_in ;
+FILE* fp_out ;
 
+void test_read (char* c) {
+	*c = fgetc (fp_in) ;
+}
+
+void test_write (char * c) {
+	fputc (*c, fp_out) ;
+}
+
+void test_rewind () {
+	rewind (fp_in) ;
+}
+
+int main (int argc, char* argv[]) {
+	if (argc < 2) return 0 ;
+	int i = 0 ;
+
+	char ascii[128] ;
+	for (i=0; i<128; ++i) {
+		ascii [i] = i;
+	}
+
+	for (i=1; i<argc; ++i) {
+		fp_in = fopen (argv[i], "r") ;
+		if (fp_in == NULL) { fprintf (stderr, "error opening file %s.\n", argv[i]) ;}
+		else {
+			fp_out = fopen ("huffman_out", "w+") ;
+			huffman_encoding (ascii, 128, 1, test_read, test_write, test_rewind) ;
+			huffman_decoding (test_read, test_write) ;
+			fclose (fp_in) ;
+			fclose (fp_out) ; 
+		}
+	} 
 }
